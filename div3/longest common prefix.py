5ff0abb77b36f9d0007f0d9c7a59e28576680820
Hermelasis <117808216+Hermelasis@users.noreply.github.com>
2024-06-20 16:35:20 +0300

    Create longest common prefix.py

---
+++ div3/longest common prefix.py
@@ -0,0 +1,11 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    strs.sort()
+    
+    for i in range(len(strs[0])):
+        if strs[0][i] != strs[-1][i]:
+            return strs[0][:i]
+
+    return strs[0]
